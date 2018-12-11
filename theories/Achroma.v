a55dc7a0d3eca82972901e6716cca0f1044c072e
Oleg Grenrus <oleg.grenrus@iki.fi>
2018-12-11 21:57:33 +0200

    Achroma laws

--- theories/Achroma.v
+++ theories/Achroma.v
@@ -1,3 +1,6 @@
+Require Import Coq.Logic.FunctionalExtensionality.
+Require Import Coq.Logic.ProofIrrelevance.
+
 Require Import LensLaws.Isomorphism.
 Require Import LensLaws.These.
 Require Import LensLaws.Crush.
@@ -46,18 +49,132 @@ Proof.
 
   refine (let (ox, b) := (f a) in g (pair ox (obb (Some b)))). Defined.
 
-(*
-Theorem achroma_complete {A B : Set} (select : A -> B) (upsert : option A -> (option B -> B) -> A):
-  1 = 1 ->
-  Achroma A B.
+Definition achroma_insert {A B} (achroma : Achroma A B) (b : B) : A.
 Proof.
-  intros law1.
+  destruct achroma as [X iso].
+  destruct iso as [f g _ _ ].
+  exact (g (pair None b)). Defined.
 
-  set (X := (option B -> B) -> A).
-  exists X.
+Definition achroma_update {A B} (achroma : Achroma A B) (a : A) (b : B) : A.
+Proof.
+  destruct achroma as [X iso].
+  destruct iso as [f g _ _ ].
+  refine (match f a with
+    | pair ox _ => g (pair ox b)
+    end). Defined.
 
-  assert (FWD_test : A -> option X * B).
-  refine (fun (a : A) => _).
-  split.
+Theorem achroma_law1 {A B} (achroma : Achroma A B) :
+  forall a b, achroma_select achroma (achroma_update achroma a b) = b.
+Proof.
+  intros a b. destruct achroma as [X ab]. destruct ab as [axb xba H H0]. crush. Qed.
 
+Theorem achroma_law2 {A B} (achroma : Achroma A B) :
+  forall a, achroma_update achroma a (achroma_select achroma a) = a.
+Proof.
+  intros a. destruct achroma as [X ab]. destruct ab as [axb xba H H0]. crush. Qed.
+
+Theorem achroma_law3 {A B} (achroma : Achroma A B) :
+  forall a b b', achroma_update achroma (achroma_update achroma a b') b = achroma_update achroma a b.
+Proof.
+  intros a b b'. destruct achroma as [X ab]. destruct ab as [axb xba H H0]. crush. Qed.
+
+Theorem achroma_law4 {A B} (achroma : Achroma A B) :
+  forall b, achroma_select achroma (achroma_insert achroma b) = b.
+Proof.
+  intros b. destruct achroma as [X ab]. destruct ab as [axb xba H H0]. crush. Qed.
+
+Definition Dec (P : Prop) : Set := {P} + {~P}.
+
+Theorem achroma_law5 {A B} (achroma : Achroma A B) :
+  forall b b', achroma_update achroma (achroma_insert achroma b) b' = achroma_insert achroma b'.
+Proof.
+  intros b b'. destruct achroma as [X ab]. destruct ab as [axb xba H H0]. crush. Qed.
+
+Theorem achroma_extra {A B} (achroma : Achroma A B) (a : A) :
+  Dec (achroma_update achroma a = achroma_insert achroma).
+Proof.
+  destruct achroma as [X ab]. destruct ab as [axb xba H H0].
+  remember (axb a) as z.
+  destruct z as [ox b].
+  unfold achroma_update. unfold achroma_insert.
+  destruct ox as [x |].
+  - right.
+    unfold not. intros.
+    assert (Haux : forall (f g : B -> A) (b : B), f = g -> f b = g b).
+    intros f g b' R. rewrite R. trivial.
+    apply Haux with (b := b) in H1.
+    rewrite <- Heqz in H1.
+    assert (Haux' : axb (xba (Some x, b)) = axb (xba (None, b))).
+    rewrite H1. trivial.
+    repeat (rewrite <- H0 in Haux').
+    inversion Haux'.
+  - left. rewrite <- Heqz. trivial. Defined.
+
+Module Completeness.
+  Parameter A : Set.
+  Parameter B : Set.
+  Parameter insert : B -> A.
+  Parameter update : A -> B -> A.
+
+  Parameter select : A -> B.
+
+  Parameter law1 : forall a b, select (update a b) = b.
+  Parameter law2 : forall a, update a (select a) = a.
+  Parameter law3 : forall a b b', update (update a b') b = update a b.
+  Parameter law4 : forall b, select (insert b) = b.
+  Parameter law5 : forall b b', update (insert b) b' = insert b'.
+
+  Parameter extra : forall a, Dec (update a = insert).
+
+  Definition X : Set := {ba : B -> A | exists (a : A), ba = update a /\ update a <> insert }.
+
+(*
+  Inductive sigma (a : A) : Set := mksigma :
+    forall (b : B), Dec (a = insert b) -> b = select a -> sigma a.
+
+  Definition select'' (a : A) : sigma a.
+  Proof.
+    remember (select' a) as b. destruct b as [b proof].
+    exists b. apply proof. unfold select. rewrite <- Heqb. exact eq_refl. Defined.
 *)
+
+  Definition FWD (a : A) : option X * B.
+  Proof.
+    destruct (extra a) as [_ | H].
+    - refine (None, select a).
+    - refine (Some _, select a).
+      exists (update a). exists a. split.
+      + exact (eq_refl).
+      + exact H. Defined.
+
+  Definition BWD (p : option X * B) : A.
+  Proof.
+    destruct p as [ox b]. destruct ox as [| x].
+    - destruct x as [ba _]. exact (ba b).
+    - exact (insert b). Defined.
+
+  Theorem achroma_compete : Achroma A B.
+  Proof.
+    exists X.
+    apply (MkIso FWD BWD); unfold FWD; unfold BWD; unfold X.
+
+    - intros a. destruct (extra a) as [E | E].
+      + rewrite <- E. symmetry. apply law2.
+      + symmetry. apply law2.
+
+    - intros p. destruct p as [ox b]. destruct ox as [x |].
+      + destruct x as [ba proof].
+        destruct proof as [a proof].
+        destruct proof as [H H'].
+        destruct (extra (ba b)) as [E | E].
+        * exfalso.
+          apply H'. rewrite <- E. rewrite -> H. extensionality b'. symmetry. apply law3.
+        * f_equal. f_equal. subst ba.
+          assert (update a = update (update a b)).
+          extensionality b'. symmetry. apply law3.
+          apply sigma_eq with (eqp := H).
+          apply proof_irrelevance. subst ba. symmetry. apply law1.
+      + destruct (extra (insert b)) as [_ | E].
+        * f_equal. symmetry. apply law4.
+        * exfalso. apply E. extensionality b'. apply law5. Qed.
+End Completeness.
